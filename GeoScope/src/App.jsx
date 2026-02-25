0d9d9bf957f6de402c89e0b823d363c642378188
Samarjeet <samarsabale1021@gmail.com>
2026-02-25 19:37:13 +0530

    feat: initialize GeoScope project with React, Vite, and Tailwind CSS
    - Add package.json with dependencies and scripts for development
    - Create main application structure with App component
    - Implement basic pages: Home, About, Contact, and Country
    - Set up global styles with Tailwind CSS
    - Configure Vite for React and Tailwind CSS integration

---
+++ GeoScope/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <>
+
+      <div className='h-screen w-screen bg-[#202020]'>
+        <Home />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ GeoScope/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-5xl font-5xl'>Home</h1>
+    </div>
+  )
+}
+
+export default Home
