e1b37ee5d2cadfcf93f70556a3fe0a1e9e4d1d46
Samarjeet <samarsabale1021@gmail.com>
2026-03-19 00:48:13 +0530

    feat: enhance Footer component with navigation links and social media icons, update HeroSection layout for improved responsiveness.

--- GeoScope/src/components/Footer.jsx
+++ GeoScope/src/components/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div>
-      <div className='w-full bg-blue-950'>
-        <h1 className='text-white'>Footer</h1>
+      <div className='w-full px-18 py-4 bg-neutral-900'>
+        <div className='w-full pb-4 flex items-center justify-between gap-6 border-b-2 border-gray-700'>
+          <div>
+            <h1 className='text-white text-2xl'>GeoScope.</h1>
+          </div>
+          <div className='text-sm text-white font-normal flex items-center gap-6'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/country'>Country</Link>
+            <Link to='/contact'>Contacts</Link>
+          </div>
+          <div>
+            <p className='text-sm text-white font-extralight cursor-pointer'>samarjeetsabale1021@gmail.com</p>
+          </div>
+        </div>
+        <div className='w-full mt-4'>
+          <div className='w-full flex items-center justify-between'>
+            <p className='text-sm text-white font-extralight'>© 2026-2027 GeoScope, all rights reserved</p>
+            <div className='flex items-center gap-2 text-white text-[1.2rem] font-extralight'>
+              <a href="https://github.com/samarjeets10"><i class="ri-github-line"></i></a>
+              <a href="https://www.linkedin.com/in/samarjeet-sabale-9254b82ba/"><i class="ri-linkedin-box-line"></i></a>
+              <a href="https://x.com/SamarjeetSabale"><i class="ri-twitter-x-line"></i></a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- GeoScope/src/components/HeroSection.jsx
+++ GeoScope/src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function HeroSection() {
   return (
     <div className='w-full min-h-[80vh] flex flex-col items-center'>
-        <div className='w-full px-[4.5rem] pb-8 pt-8 flex flex-col text-center items-center justify-center sm:flex-col md:flex-row lg:flex-row md:text-start lg:text-start  gap-[4rem] '>
+        <div className='w-full px-[4.5rem] pb-8 pt-8 flex flex-col text-center items-center justify-center sm:flex-col md:flex-row lg:flex-row md:text-start lg:text-start gap-[4rem]'>
             <div className='w-[60%] flex flex-col gap-4'>
                 <h2 className='text-[3.5vw] font-semibold leading-13'>Explore the World, One Country at a Time.</h2>
                 <p className='text-md text-white'>Navigate the globe through a lens of precision. Your curated gateway to real-time country data and worldwide intelligence.</p>
